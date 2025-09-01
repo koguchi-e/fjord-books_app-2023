6f28cafa3ca4d86c617fc1a8f767f0062d0cd31a
koguchi-e <koguchi.e915@gmail.com>
2025-09-01 17:20:37 +0900

    登録・更新・削除成功メッセージを変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[show edit update destroy]
+  before_action :set_book, only: %i[ show edit update destroy ]
 
   # GET /books or /books.json
   def index
@@ -9,7 +7,8 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1 or /books/1.json
-  def show; end
+  def show
+  end
 
   # GET /books/new
   def new
@@ -17,7 +16,8 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1/edit
-  def edit; end
+  def edit
+  end
 
   # POST /books or /books.json
   def create
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: t('book.create') }
+        format.html { redirect_to book_url(@book), notice: t("notice.create", title: @book.title) }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: t('book.create') }
+        format.html { redirect_to book_url(@book), notice: t("notice.update", title: @book.title)}
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,20 +52,19 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: t('book.destroy') }
+      format.html { redirect_to books_url, notice: t("notice.destroy", title: @book.title) }
       format.json { head :no_content }
     end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_book
-    @book = Book.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def book_params
-    params.require(:book).permit(:title, :memo, :author, :picture)
-  end
+    # Only allow a list of trusted parameters through.
+    def book_params
+      params.require(:book).permit(:title, :memo, :author, :picture)
+    end
 end
