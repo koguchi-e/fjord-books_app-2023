b6d755e93b5e7831176f87ad6781a228c42c84a2
koguchi-e <koguchi.e915@gmail.com>
2025-09-01 15:11:15 +0900

    エラーメッセージの日本語化対応

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@
 
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
+  # 動作確認のため一時的に追加
+  validates :title,  presence: true
+  validates :author, presence: true
+  validates :memo,   presence: true
 end
