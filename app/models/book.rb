aef1a1da7d20b98ac1635d389519e3102322b86f
koguchi-e <koguchi.e915@gmail.com>
2025-09-02 11:00:50 +0900

    英語画面用ページタイトル設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,4 @@
 
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
-  # 動作確認のため一時的に追加
-  validates :title,  presence: true
-  validates :author, presence: true
-  validates :memo,   presence: true
 end
