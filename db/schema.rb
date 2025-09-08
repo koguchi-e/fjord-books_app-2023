0af044c3cd3d4894ad8fa5fc8c58f3b9adbaddef
koguchi-e <koguchi.e915@gmail.com>
2025-09-08 14:21:19 +0900

    Nameカラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_08_033728) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
