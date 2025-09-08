ba6f83cf8dd2bfe602688394624194f41a97442a
koguchi-e <koguchi.e915@gmail.com>
2025-09-08 14:48:35 +0900

    ユーザーの郵便番号、住所と自己紹介文カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_09_08_033728) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_08_054632) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -29,6 +29,9 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_08_033728) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "psot_code"
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
