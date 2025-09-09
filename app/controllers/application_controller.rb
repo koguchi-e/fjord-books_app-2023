275c3f92d9279a70a5826d63b309bbfcf10a2b06
koguchi-e <koguchi.e915@gmail.com>
2025-09-09 14:35:57 +0900

    画面日英対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,19 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :switch_locale
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :postal_code, :introduction])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :postal_code, :introduction])
+  end
+
+  private
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 end
