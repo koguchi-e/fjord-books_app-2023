21b56df76ec353d0b1ef6414b31149970e697da9
koguchi-e <koguchi.e915@gmail.com>
2025-09-10 11:41:05 +0900

    デフォルトを日本語に変更

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module BooksApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
     config.i18n.available_locales = %i[en ja]
   end
 end
