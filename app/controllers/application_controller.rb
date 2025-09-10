f4b3171e6320d8a65168c64312d2c208e1b5bb91
koguchi-e <koguchi.e915@gmail.com>
2025-09-10 14:02:16 +0900

    編集画面/ 削除ボタンなどの日本語訳

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,16 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :require_login
   around_action :switch_locale
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :postal_code, :introduction])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :postal_code, :introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :post_code, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :post_code, :introduction])
   end
 
   private
-
-  # def require_login
-  #   unless logged_in?
-  #     flash[:error] = "このページにアクセスするにはログインが必要です。"
-  #     redirect_to new_login_url
-  #   end
-  # end
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
