00df3a10137bbf48d6f4d778ea158dcd6b5348cc
koguchi-e <koguchi.e915@gmail.com>
2025-09-17 11:16:58 +0900

    jpg, png, gif以外の形式ではエラーになるよう変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  validate :avatar_content_type
+  
+  def avatar_content_type
+    if avatar.attached? && !avatar.content_type.in?(%w[avatar/jpeg avatar/png avatar/gif])
+      errors.add(:avatar, 'ファイル形式は、jpg/png/gifのいずれかにしてください。')
+    end
+  end
 end
