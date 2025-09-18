ac68c6152c99e8c9ee5d7192fa72e3cdf1446920
koguchi-e <koguchi.e915@gmail.com>
2025-09-18 14:38:26 +0900

    indexアクションをallなしで書くように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
 
   def index
-    @users = User.all.order(created_at: :asc).page(params[:page])
+    @users = User.order(created_at: :asc).page(params[:page])
   end
 
   def show; end
