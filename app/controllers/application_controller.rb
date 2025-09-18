2e46ca6fcbda33fe9728c85b40f9c95d25b7da13
koguchi-e <koguchi.e915@gmail.com>
2025-09-18 11:49:50 +0900

    before_action :authenticate_userをApplicationController に移す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   around_action :switch_locale
+  before_action :authenticate_user!
 
   protected
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@
 
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
-  before_action :authenticate_user!
 
   def index
     @users = User.all.order(created_at: :asc).page(params[:page])
