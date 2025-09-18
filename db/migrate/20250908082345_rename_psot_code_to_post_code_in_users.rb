cd699364e09e304dbca4cf8f87caf269458e5632
koguchi-e <koguchi.e915@gmail.com>
2025-09-18 11:42:38 +0900

    post_codeカラムの誤字修正用のマイグレーションをコメントアウト

--- db/migrate/20250908082345_rename_psot_code_to_post_code_in_users.rb
+++ db/migrate/20250908082345_rename_psot_code_to_post_code_in_users.rb
@@ -1,5 +1,5 @@
 class RenamePsotCodeToPostCodeInUsers < ActiveRecord::Migration[7.0]
   def change
-    rename_column :users, :psot_code, :post_code
+    # rename_column :users, :psot_code, :post_code
   end
 end
