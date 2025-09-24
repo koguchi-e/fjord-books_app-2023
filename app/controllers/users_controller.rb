5e8ce45c6a37bf3b9079f81521922c9f1de879d3
koguchi-e <koguchi.e915@gmail.com>
2025-09-24 14:28:37 +0900

    User.findの処理をshowアクション内にまとめる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
 
   def index
     @users = User.order(created_at: :asc).page(params[:page])
   end
 
-  def show; end
-
-  private
-
-  def set_user
+  def show
     @user = User.find(params[:id])
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :post_code, :address, :introduction)
