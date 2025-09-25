9a8a30a0e31673c9828e67ead16eb203a51b0c22
koguchi-e <koguchi.e915@gmail.com>
2025-09-25 17:41:09 +0900

    コメント機能フォーム作成・未完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :reports
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+  resources :reports do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
 end
