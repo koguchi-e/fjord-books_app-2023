b2a4d353673acd3c8cd2b1d9bfa49be0b73c62f7
koguchi-e <koguchi.e915@gmail.com>
2025-09-25 15:34:28 +0900

    ビューにCSSが適用されるよう修正

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
