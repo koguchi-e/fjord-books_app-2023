8f89a78587a061a222be997ef49ae067900cc46a
koguchi-e <koguchi.e915@gmail.com>
2025-10-16 11:23:32 +0900

    reportテーブルのuserをreferencesに変更

--- db/migrate/20250924083414_create_reports.rb
+++ db/migrate/20250924083414_create_reports.rb
@@ -3,7 +3,7 @@ class CreateReports < ActiveRecord::Migration[7.0]
     create_table :reports do |t|
       t.text :title, null: false
       t.text :body, null: false
-      t.integer "user_id", null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_25_081719) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -86,4 +87,5 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_25_081719) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "reports", "users"
 end
