35b874ece7e5f7cc2d1b520578c18d19439de189
koguchi-e <koguchi.e915@gmail.com>
2025-10-16 15:32:31 +0900

    inverse_ofを使って双方向関連付けを行う

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,12 +5,24 @@ class Report < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
 
   # 自分が言及する側・その日報が言及している日報たち（言及先）・ing
-  has_many :mention_from_me, class_name: 'ReportMention', foreign_key: :source_report_id, dependent: :destroy
-  has_many :mentioned_reports, through: :mention_from_me, source: :target_report
+  has_many  :mention_from_me,
+            class_name: 'ReportMention',
+            foreign_key: :source_report_id,
+            dependent: :destroy,
+            inverse_of: :source_report
+  has_many  :mentioned_reports,
+            through: :mention_from_me,
+            source: :target_report
 
   # 自分が言及される側・その日報に言及している日報たち（言及元）・ned
-  has_many :mention_to_me, class_name: 'ReportMention', foreign_key: :target_report_id, dependent: :destroy
-  has_many :mentioning_reports, through: :mention_to_me, source: :source_report
+  has_many  :mention_to_me,
+            class_name: 'ReportMention',
+            foreign_key: :target_report_id,
+            dependent: :destroy,
+            inverse_of: :target_report
+  has_many  :mentioning_reports,
+            through: :mention_to_me,
+            source: :source_report
 
   validates :title, presence: true
   validates :content, presence: true
