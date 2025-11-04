0de3494f2e5437d37205f64a5d1d5618446f0a0f
koguchi-e <koguchi.e915@gmail.com>
2025-11-04 13:39:54 +0900

    ユーザーのname_or_emailメソッドのテストを書く

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test 'name_or_email return name if present' do
+    user = User.new(name: '山田太郎', email: 'yamada@example.com')
+    assert_equal '山田太郎', user.name_or_email
+  end
+  test 'name_or_email retrn email if name is blank' do
+    user = User.new(name: '', email: 'yamada@example.com')
+    assert_equal 'yamada@example.com', user.name_or_email
+  end
 end
