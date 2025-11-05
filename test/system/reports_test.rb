8127511cb7125ceeb38a2d8e202558b6a692a2d5
koguchi-e <koguchi.e915@gmail.com>
2025-11-05 11:47:27 +0900

    日報の編集・削除のテストコードを追加

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -9,9 +9,9 @@ class ReportsTest < ApplicationSystemTestCase
 
   fixtures :users
 
-  test 'ログインして新規に日報を作成する' do
+  def login_as_testuser
     visit root_url
-    fill_in 'Eメール', with: 'yamada@example.com'
+    fill_in 'Eメール', with: 'test_user@example.com'
     fill_in 'パスワード', with:'password'
 
     click_button 'ログイン'
@@ -23,6 +23,10 @@ class ReportsTest < ApplicationSystemTestCase
 
     visit reports_url
     assert_selector 'h1', text: '日報の一覧'
+  end
+
+  test 'ログインして新規に日報を作成する' do
+    login_as_testuser
 
     click_on '日報の新規作成'
     fill_in 'タイトル', with: '2日目'
@@ -33,6 +37,34 @@ class ReportsTest < ApplicationSystemTestCase
     visit report_url(Report.last)
     assert_text '2日目'
     assert_text 'テストコードは楽しい、設計が苦手だと気づく'
+  end
+
+  test '日報の編集' do
+    login_as_testuser
+    sleep 3
+    click_on 'この日報を表示'
+
+    click_on 'この日報を編集'
+    fill_in 'タイトル', with: '3日目'
+    fill_in '内容', with: 'RubyGoldが難しい！'
+
+    click_button '更新する'
+    assert_text '日報が更新されました。'
+
+    visit report_url(Report.last)
+    assert_text '3日目'
+    assert_text 'RubyGoldが難しい！'
+  end
+
+  test '日報の削除' do
+    login_as_testuser
+    click_on 'この日報を表示'
+
+    click_button 'この日報を削除'
+    assert_text '日報が削除されました。'
+    visit reports_url
+    assert_no_text '１日目'
+    assert_no_text 'http://localhost:3000/reports/1'
   end
 
   # test 'visiting the index' do
