e38800acc65b8a0208b062700f4d58aa0f287327
koguchi-e <koguchi.e915@gmail.com>
2025-11-05 10:28:52 +0900

    日報一覧にアクセスまでシステムテストを追加

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -3,45 +3,62 @@
 require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
-  setup do
-    @report = reports(:one)
-  end
+  # setup do
+  #   @report = reports(:one)
+  # end
 
-  test 'visiting the index' do
+  fixtures :users
+
+  test 'ログインして日報の一覧にアクセスする' do
+    visit root_url
+    fill_in 'Eメール', with: 'yamada@example.com'
+    fill_in 'パスワード', with:'password'
+    click_button 'ログイン'
+    assert_text 'ログインしました。'
+
+    visit books_url
+    assert_selector 'h1', text: '本の一覧'
+    click_on '日報'
     visit reports_url
-    assert_selector 'h1', text: 'Reports'
+    assert_selector 'h1', text: '日報の一覧'
+    sleep 3
   end
 
-  test 'should create report' do
-    visit reports_url
-    click_on 'New report'
+  # test 'visiting the index' do
+  #   visit reports_url
+  #   assert_selector 'h1', text: 'Reports'
+  # end
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
+  # test 'should create report' do
+  #   visit reports_url
+  #   click_on 'New report'
 
-    assert_text 'Report was successfully created'
-    click_on 'Back'
-  end
+  #   fill_in 'Content', with: @report.content
+  #   fill_in 'Title', with: @report.title
+  #   fill_in 'User', with: @report.user_id
+  #   click_on 'Create Report'
 
-  test 'should update Report' do
-    visit report_url(@report)
-    click_on 'Edit this report', match: :first
+  #   assert_text 'Report was successfully created'
+  #   click_on 'Back'
+  # end
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
+  # test 'should update Report' do
+  #   visit report_url(@report)
+  #   click_on 'Edit this report', match: :first
 
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
-  end
+  #   fill_in 'Content', with: @report.content
+  #   fill_in 'Title', with: @report.title
+  #   fill_in 'User', with: @report.user_id
+  #   click_on 'Update Report'
 
-  test 'should destroy Report' do
-    visit report_url(@report)
-    click_on 'Destroy this report', match: :first
+  #   assert_text 'Report was successfully updated'
+  #   click_on 'Back'
+  # end
 
-    assert_text 'Report was successfully destroyed'
-  end
+  # test 'should destroy Report' do
+  #   visit report_url(@report)
+  #   click_on 'Destroy this report', match: :first
+
+  #   assert_text 'Report was successfully destroyed'
+  # end
 end
