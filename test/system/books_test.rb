d0a4a84bba1c13c3bf0daa8e8fd6c63f2b6c2155
koguchi-e <koguchi.e915@gmail.com>
2025-11-05 17:32:15 +0900

    本のシステムテストを修正

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -1,54 +1,62 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# require 'application_system_test_case'
+require 'application_system_test_case'
 
-# class BooksTest < ApplicationSystemTestCase
-#   def fill_in_book_information
-#     fill_in 'タイトル', with: '吾輩は猫である'
-#     fill_in 'メモ', with: '吾輩わがはいは猫である。'
-#     fill_in '著者', with: '夏目漱石'
-#     attach_file 'book[picture]', Rails.root.join('test/fixtures/files/cat.png')
-#   end
+class BooksTest < ApplicationSystemTestCase
+  def create_book
+    click_on '本の新規作成'
+    fill_in 'タイトル', with: '吾輩は猫である'
+    fill_in 'メモ', with: '吾輩わがはいは猫である。'
+    fill_in '著者', with: '夏目漱石'
+    attach_file 'book[picture]', Rails.root.join('test/fixtures/files/cat.png')
 
-#   def check_book_information
-#     assert_text '吾輩は猫である'
-#     assert_text '吾輩わがはいは猫である。'
-#     assert_text '夏目漱石'
-#     assert_selector "img[src*='cat.png']"
-#   end
+    click_button '登録する'
+    assert_text '本が作成されました。'
+  end
 
-#   test 'ログインして本を新規作成' do
-#     login_as_testuser
-#     click_on '本の新規作成'
-#     fill_in_book_information
-#     click_button '登録する'
+  test 'ログインして本を新規作成' do
+    login_as_testuser
+    create_book
 
-#     assert_text '本が作成されました。'
-#     visit book_url(Book.last)
-#     check_book_information
-#   end
+    visit book_url(Book.last)
+    assert_text '吾輩は猫である'
+    assert_text '吾輩わがはいは猫である。'
+    assert_text '夏目漱石'
+    assert_selector "img[src*='cat.png']"
+  end
 
-#   test '本の編集' do
-#     login_as_testuser
-#     click_on 'この本を表示'
-#     click_on 'この本を編集'
-#     fill_in_book_information
-#     click_button '更新する'
+  test '本の編集' do
+    login_as_testuser
+    create_book
+    visit books_url
+    click_on 'この本を表示'
+    click_on 'この本を編集'
 
-#     assert_text '本が更新されました。'
-#     visit book_url(Book.last)
-#     check_book_information
-#   end
+    fill_in 'タイトル', with: '吾輩は猫である2'
+    fill_in 'メモ', with: '続編'
+    fill_in '著者', with: '夏目漱石2世'
+    attach_file 'book[picture]', Rails.root.join('test/fixtures/files/girl.png')
+    click_button '更新する'
 
-#   test '本の削除' do
-#     login_as_testuser
-#     click_on 'この本を表示'
-#     click_button 'この本を削除'
+    assert_text '本が更新されました。'
+    visit book_url(Book.last)
+    assert_text '吾輩は猫である2'
+    assert_text '続編'
+    assert_text '夏目漱石2世'
+    assert_selector "img[src*='girl.png']"
+  end
 
-#     assert_text '本が削除されました。'
-#     visit books_url
-#     assert_no_text '山月記'
-#     assert_no_text '隴西ろうさいの李徴りちょうは博学才穎さいえい、'
-#     assert_no_text '中島敦'
-#   end
-# end
+  test '本の削除' do
+    login_as_testuser
+    create_book
+    visit books_url
+    click_on 'この本を表示'
+    click_button 'この本を削除'
+
+    assert_text '本が削除されました。'
+    visit books_url
+    assert_no_text '吾輩は猫である'
+    assert_no_text '吾輩わがはいは猫である。'
+    assert_no_text '夏目漱石'
+  end
+end
