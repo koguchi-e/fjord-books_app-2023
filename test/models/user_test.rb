65c4cb732c6402e2cae67f865abaaebe6e8caffb
koguchi-e <koguchi.e915@gmail.com>
2025-11-05 14:52:46 +0900

    リファクタリング時のミスを修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'name_or_emailが名前を返すか' do
     user = users(:user)
-    assert_equal '山田太郎', user.name_or_email
+    assert_equal 'テストユーザー', user.name_or_email
   end
 
   test 'name_or_emailがもし名前が空欄ならメールアドレスを返すか' do
