f052a71e44a7f9819163eb3d5344f997cd033d5a
koguchi-e <koguchi.e915@gmail.com>
2025-11-05 11:51:47 +0900

    システムテストのコメントアウトしてた箇所を削除

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -3,10 +3,6 @@
 require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
-  # setup do
-  #   @report = reports(:one)
-  # end
-
   fixtures :users
 
   def login_as_testuser
@@ -66,42 +62,4 @@ class ReportsTest < ApplicationSystemTestCase
     assert_no_text '１日目'
     assert_no_text 'http://localhost:3000/reports/1'
   end
-
-  # test 'visiting the index' do
-  #   visit reports_url
-  #   assert_selector 'h1', text: 'Reports'
-  # end
-
-  # test 'should create report' do
-  #   visit reports_url
-  #   click_on 'New report'
-
-  #   fill_in 'Content', with: @report.content
-  #   fill_in 'Title', with: @report.title
-  #   fill_in 'User', with: @report.user_id
-  #   click_on 'Create Report'
-
-  #   assert_text 'Report was successfully created'
-  #   click_on 'Back'
-  # end
-
-  # test 'should update Report' do
-  #   visit report_url(@report)
-  #   click_on 'Edit this report', match: :first
-
-  #   fill_in 'Content', with: @report.content
-  #   fill_in 'Title', with: @report.title
-  #   fill_in 'User', with: @report.user_id
-  #   click_on 'Update Report'
-
-  #   assert_text 'Report was successfully updated'
-  #   click_on 'Back'
-  # end
-
-  # test 'should destroy Report' do
-  #   visit report_url(@report)
-  #   click_on 'Destroy this report', match: :first
-
-  #   assert_text 'Report was successfully destroyed'
-  # end
 end
